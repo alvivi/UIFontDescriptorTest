20a0328bac33462f90945a1935ca505123260490
Alvaro Vilanova Vidal <alvivi@gmail.com>
2016-01-22 20:47:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Comparing UIFont constructors
+
--- uifontdescriptortest/ViewController.swift
+++ uifontdescriptortest/ViewController.swift
@@ -1,25 +1,32 @@
-//
-//  ViewController.swift
-//  uifontdescriptortest
-//
-//  Created by Alvaro Vilanova Vidal on 22/1/16.
-//  Copyright © 2016 hacking.bar. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var firstLabel: UILabel!
+  @IBOutlet weak var secondLabel: UILabel!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBAction func onStartTest(sender: AnyObject) {
+    startMeasurement()
+    let firstFont = UIFont(name: "Marker Felt", size: 16)
+    firstLabel.font = firstFont
+    finishMeasurement("UIFont(name)")
+    
+    startMeasurement()
+    let secondFontDescriptor = UIFontDescriptor(name: "Marker Felt", size: 16)
+    let secondFont = UIFont(descriptor: secondFontDescriptor, size: 16)
+    secondLabel.font = secondFont
+    finishMeasurement("UIFont(descriptor)")
   }
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+private var time: UInt64 = 0
 
+public func startMeasurement() {
+  time = mach_absolute_time()
+}
 
+public func finishMeasurement(name: String) {
+  let duration = mach_absolute_time() - time
+  print("* \(name) \(duration)ns")
 }
 
